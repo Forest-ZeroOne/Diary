24ad93db7b800af3f175c24d92ae9f2037badf1b
LinYi <550582668@qq.com>
2021-02-27 14:04:23 +0800

    diary_01

---
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.security.SecureRandom;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "tag";
+    FloatingActionButton btn;
+    TextView tv;
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (FloatingActionButton) findViewById(R.id.fab);
+        tv = findViewById(R.id.tv);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,EditActivity.class);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    //接收startActivityForResult的结果
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        String edit = data.getStringExtra("input");
+        tv.setText(edit);
+    }
+}
